16d0ba0301e109786f8976199901fd67d99c8b26
Seth McGinnis <mcginnis@ucar.edu>
2024-02-28 14:39:37 -0700

    added checks for argument (non-)existence, cleaned up docstring

--- scripts/zarrify.py
+++ scripts/zarrify.py
@@ -1,28 +1,52 @@
-"""
+#!/usr/bin/env python
+"""Converts a directory of netcdf files into a single zarr store.
+
+Usage:
+    zarrify.py path/to/input/dir outname.zarr
+
+path/to/input/dir must contain one or more .nc files, and outname.zarr
+must not already exist.
+
+
+On Casper, zarrifying 1 year of hourly conus404 data (1 variable,
+1015x1367 = 1.39e6 gridpoints, 8760 timesteps, total data volume ~
+25GB) takes about 10 minutes wallclock and requires about 300MB of
+memory.
+
+
 Author: Seth McGinnis
 Contact: mcginnis@ucar.edu
-
-This script converts a directory of netcdf files into a single zarr store.
 
 # Dependencies:
 # - xarray
 # - sys
-# 
-# Example use:
-# python zarrify.py data/T2/2020 zarr/T2/T2.2020.zarr
-# 
+# - os.path
+# - glob
+
 """
 import xarray
 import sys
+import os.path
+import glob
 
 if(len(sys.argv) != 3):
+    print("error: not enough arguments")
     print("Usage: python zarrify.py path/to/input/dir outname.zarr")
-    print("creates outname.zarr from all .nc files in indir")
+    print("       creates outname.zarr from all .nc files in indir")
     quit()
 
 indir = sys.argv[1]
 inglob = indir + "/*.nc"
 outzarr = sys.argv[2]
+
+if(os.path.exists(os.path.expanduser(outzarr))):
+    print("error: outname.zarr must not already exist")
+    quit()
+
+if(len(glob.glob(os.path.expanduser(inglob))) < 1):
+    print("error: input directory contains no .nc files")
+    quit()
+
 
 ds = xarray.open_mfdataset(inglob)
 
