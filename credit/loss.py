f02552171efffb71d4a2f8967206d1fa78472d28
dkimpara <dkimpara@ucar.edu>
2024-02-29 14:35:31 -0700

    reworked spectralLoss2D to be compatible with new models

--- credit/loss.py
+++ credit/loss.py
@@ -115,33 +115,54 @@ class SpectralLoss2D(torch.nn.Module):
         self.wavenum_init = wavenum_init
         self.reduction = reduction
 
-    def forward(self, output, target, weights=None, fft_dim=3):
+    def forward(self, output, target, weights=None, fft_dim=-1):
+        # code is currently for (..., lat, lon)
+        # todo: write for  (... lat, lon, ... ) 
 
         device, dtype = output.device, output.dtype
         output = output.float()
         target = target.float()
 
         # Take FFT over the 'lon' dimension
+        # (B, c, lat, lon)
         out_fft = torch.fft.rfft(output, dim=fft_dim)
         target_fft = torch.fft.rfft(target, dim=fft_dim)
+        # (B, c, lat, wavenum)
 
         # Take absolute value
         out_fft_abs = torch.abs(out_fft)
         target_fft_abs = torch.abs(target_fft)
 
         if weights is not None:
-            weights = weights.unsqueeze(1).permute(0, 3, 1, 2).to(device=device, dtype=dtype)
+            #weights.shape = (1, lat, 1)
+            weights = weights.permute(0, 2, 1).to(device=device, dtype=dtype)
+            # (1, 1, 1, lat), matmul will broadcast as long as last dim is lat
             out_fft_abs = torch.matmul(weights, out_fft_abs)
             target_fft_abs = torch.matmul(weights, target_fft_abs)
+            # matmul broadcasts over dims except last two, where it does a matrix mult
+            # (1, 1, 1, lat) x (B, c, T, lat, wavenum)
+            # does multiplication on submatrices (2d tensors) defined by last two dims
+            # result: (B, c, T, 1, wavenum), weighted sum over all wavenums 
+            # would probably be clearer to rewrite this using torch.mean and weight vector
+            
+            # probably want to normalize by the norm of the lat weights 
+            # so divide everything by |lat| to get a true average
+            out_fft_abs = out_fft_abs / weights.shape[-1]
+            target_fft_abs = target_fft_abs / weights.shape[-1]
 
         # Average over spatial dims
-        out_fft_mean = torch.mean(out_fft_abs, dim=(fft_dim - 1))
-        target_fft_mean = torch.mean(target_fft_abs, dim=(fft_dim - 1))
+        # out_fft_mean = torch.mean(out_fft_abs, dim=(fft_dim - 1))
+        # target_fft_mean = torch.mean(target_fft_abs, dim=(fft_dim - 1))
+        # mean op can be replaced by a squeeze, since it is mean over dim of size 1
+        
+        # removing lat dim, since its now averaged
+        out_fft_mean = out_fft_abs.squeeze(fft_dim - 1) 
+        target_fft_mean = target_fft_abs.squeeze(fft_dim - 1)
+        # (B, c, T, wavenum)
 
-        # Compute MSE
-        loss = torch.square(out_fft_mean[:, :, self.wavenum_init:] - target_fft_mean[:, :, self.wavenum_init:])
-        loss = torch.sqrt(loss.mean())
-
+        # Compute MSE, no sqrt according to FouRKS paper/ repo
+        loss = torch.square(out_fft_mean[..., self.wavenum_init:] - target_fft_mean[..., self.wavenum_init:])
+        loss = loss.mean()
         return loss.to(device=device, dtype=dtype)
 
 
