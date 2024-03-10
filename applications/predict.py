e912d8ad0a9c69cb8874ed8c6d1f2df126a1e331
Yingkai Sha <yingkaisha@gmail.com>
2024-03-10 17:21:56 -0600

    draw_forecast now produces outputs for multiple upper air levels

--- applications/predict.py
+++ applications/predict.py
@@ -55,15 +55,7 @@ from credit.seed import seed_everything
 from credit.pbs import launch_script, launch_script_mpi
 
 # ---------- #
-# Graph
 from visualization import draw_forecast
-# import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
-# from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-
-# import cartopy.crs as ccrs
-# import cartopy.mpl.geoaxes
-# import cartopy.feature as cfeature
 
 # import wandb
 
@@ -77,93 +69,6 @@ os.environ["MKL_NUM_THREADS"] = "1"
 def setup(rank, world_size, mode):
     logging.info(f"Running {mode.upper()} on rank {rank} with world_size {world_size}.")
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
-
-
-# def draw_forecast(data, N_level=15, level_num=10, var_num=4, 
-#                   conf=None, times=None, forecast_count=None, save_location=None):
-#     '''
-#     This function produces 4-panel figures 
-#     '''
-#     # ------------------------------ #
-#     # visualization settings
-#     ## variable range limit with units of m/s, m/s, K, g/kg
-#     var_lims = [[-20, 20], [-20, 20], [273.15-35, 273.15+35], [0, 1e-2]]
-#     ## colormap
-#     colormaps = [plt.cm.Spectral, plt.cm.Spectral, plt.cm.RdBu_r, plt.cm.YlGn]
-#     ## colorbar extend
-#     colorbar_extends = ['both', 'both', 'both', 'max']
-#     ## title
-#     title_strings = ['U wind [m/s]; level {}\ntime: {}; step: {}', 
-#                      'V wind [m/s]; level {}\ntime: {}; step: {}', 
-#                      'Air temperature [$^\circ$K]; level {}\ntime: {}; step: {}', 
-#                      'Specific humidity [kg/kg]; level {}\ntime: {}; step: {}']
-#     # ------------------------------ #
-#     # get forecast step and file name
-#     k, fn = data
-#     t = times[k]
-#     pred = np.load(fn)
-    
-#     # ------------------------------ #
-#     # get lat/lon grids
-#     lat_lon_weights = xr.open_dataset(conf['loss']['latitude_weights'])
-#     longitude = lat_lon_weights["longitude"]
-#     latitude = lat_lon_weights["latitude"]
-    
-#     # ------------------------------ #
-#     # Figure
-#     fig = plt.figure(figsize=(13, 6.5))
-    
-#     # 2-by-2 subplots
-#     gs = gridspec.GridSpec(2, 2, height_ratios=[1, 1], width_ratios=[1, 1])
-#     proj_ = ccrs.EckertIII()
-    
-#     # subplot ax
-#     ax0 = plt.subplot(gs[0, 0], projection=proj_)
-#     ax1 = plt.subplot(gs[0, 1], projection=proj_)
-#     ax2 = plt.subplot(gs[1, 0], projection=proj_)
-#     ax3 = plt.subplot(gs[1, 1], projection=proj_)
-#     AX = [ax0, ax1, ax2, ax3]
-#     plt.subplots_adjust(0, 0, 1, 1, hspace=0.2, wspace=0.05)
-    
-#     # lat/lon gridlines and labeling
-#     for ax in AX:
-#         GL = ax.gridlines(crs=ccrs.PlateCarree(), 
-#                           draw_labels=True, x_inline=False, y_inline=False, 
-#                           color='k', linewidth=0.5, linestyle=':', zorder=5)
-#         GL.top_labels = None; GL.bottom_labels = None
-#         GL.right_labels = None; GL.left_labels = None
-#         GL.xlabel_style = {'size': 14}; GL.ylabel_style = {'size': 14}
-#         GL.rotate_labels = False
-    
-#         ax.add_feature(cfeature.COASTLINE.with_scale('110m'), edgecolor='k', linewidth=1.0, zorder=5)
-#         ax.spines['geo'].set_linewidth(2.5)
-
-#     # pcolormesh / colorbar / title in loops
-#     for i_var in range(var_num):
-#         # get the current axis
-#         ax = AX[i_var]
-#         # get the current variable
-#         var_ind = i_var*N_level + level_num
-#         pred_draw = pred[var_ind]
-#         # get visualization settings
-#         var_lim = var_lims[i_var]
-#         colormap = colormaps[i_var]
-#         cbar_extend = colorbar_extends[i_var]
-#         # pcolormesh
-#         cbar = ax.pcolormesh(longitude, latitude, pred_draw, vmin=var_lim[0], vmax=var_lim[1], 
-#                              cmap=colormap, transform=ccrs.PlateCarree())
-#         # colorbar operations
-#         CBar = fig.colorbar(cbar, location='right', orientation='vertical', 
-#                             pad=0.02, fraction=0.025, shrink=0.6, aspect=15, extend=cbar_extend, ax=ax)
-#         CBar.ax.tick_params(axis='y', labelsize=14, direction='in', length=0)
-#         CBar.outline.set_linewidth(2.5)
-#         # title
-#         ax.set_title(title_strings[i_var].format(level_num, t, k), fontsize=14)
-    
-#     filename = join(save_location, f"global_q_{forecast_count}_{k}.png")
-#     plt.savefig(filename, dpi=300, bbox_inches="tight")
-#     plt.close()
-#     return k, filename
 
 def predict(rank, world_size, conf):
 
@@ -315,20 +220,40 @@ def predict(rank, world_size, conf):
                 forecast_paths = os.path.join(save_location, f"{forecast_count}_*_*_pred.npy")
                 # generator of file_name, file_count
                 file_list = enumerate(sorted(glob.glob(forecast_paths)))
-                # parallelize draw_forecast func
+
+                # =============================================================================== #
+                # Data visualization for upper air variables
+                upper_air_levels = conf['visualization']['upper_air_visualize']['visualize_levels']
+                
+                ## parallelize draw_forecast func
                 with Pool(processes=8) as pool:
                     f = partial(draw_forecast, conf=conf, times=forecast_datetimes,
                                 forecast_count=forecast_count, save_location=save_location)
                     # collect output png file names
                     video_files = pool.map(f, file_list)
                     
-                # generate gif
-                video_files = [x[1] for x in sorted(video_files)]
-                command_str = f'convert -delay 20 -loop 0 {" ".join(video_files)} {save_location}/global.gif'
+                ## collect all png file names
+                ## video_files[0] = f; video_files[1] = file_names
+                video_files_all = [x[1] for x in sorted(video_files)]
                 
-                out = subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-                print(out)
+                ## gif outpout name = png output name
+                gif_name_prefix = conf['visualization']['upper_air_visualize']['file_name_prefix']
                 
+                ## separate file names based on verticial levels
+                ## one gif per level 
+                for n_gif, upper_air_level in enumerate(upper_air_levels):
+                    video_files_signle = []
+                    for filename_list in video_files_all:
+                        video_files_signle.append(filename_list[n_gif])
+                        
+                    ## send all png files to the gif maker 
+                    gif_name = '{}_level{:02d}.gif'.format(gif_name_prefix, upper_air_level)
+                    command_str = f'convert -delay 20 -loop 0 {" ".join(video_files_signle)} {save_location}/{gif_name}'
+                    out = subprocess.Popen(command_str, shell=True, 
+                                           stdout=subprocess.PIPE, 
+                                           stderr=subprocess.PIPE).communicate()
+                    print(out)
+                # =============================================================================== #
                 forecast_count += 1
                 metrics_results = defaultdict(list)
                 pred_files = []
--- applications/visualization.py
+++ applications/visualization.py
@@ -13,37 +13,93 @@ from os.path import join
 # Numerics
 import datetime
 import numpy as np
-#import pandas as pd
 import xarray as xr
-
 # ---------- #
 # matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib import colormaps as plt_cmaps
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 # cartopy
 import cartopy.crs as ccrs
 import cartopy.mpl.geoaxes
 import cartopy.feature as cfeature
 
-def draw_forecast(data, N_level=15, level_num=10, var_num=4, 
-                  conf=None, times=None, forecast_count=None, save_location=None):
+
+def get_colormap(cmap_strings):
+    colormap_obj = []
+    for cmap_name in cmap_strings:
+        colormap_obj.append(plt_cmaps[cmap_name])
+    return colormap_obj
+
+def get_colormap_extend(var_range):
+    if var_range[0] == 0.0:
+        return 'max'
+    elif var_range[1] == 0.0:
+        return 'min'
+    else:
+        return 'both'
+    return colorbar_extends
+
+def get_variable_range(data):
+    data_ravel = data.ravel()
+    
+    data_max = np.quantile(data_ravel, 0.98)
+    if np.abs(np.min(data)) < 1e-2:
+        data_min = 0
+    else:
+        data_min = np.quantile(data_ravel, 0.02)
+        
+    # rounding
+    if data_max > 10:
+        round_val = 10
+    elif data_max > 1:
+        round_val = 1
+    elif data_max > 0.1:
+        round_val = 0.1
+    elif data_max > 0.01:
+        round_val = 0.01
+    elif data_max > 0.001:
+        round_val = 0.001
+    elif data_max > 0.0001:
+        round_val = 0.0001
+    else:
+        round_val = 0.00001
+        
+    data_max = int(np.ceil(data_max / round_val)) * round_val
+    if data_min != 0:
+        data_min = int(np.floor(data_min / round_val)) * round_val
+        
+    # 0 in the middle
+    if data_min < 0 and data_max > 0:
+        data_limit = np.max([-data_min, data_max])
+        data_min = -data_limit
+        data_max = data_limit
+    
+    return [data_min, data_max]
+
+def draw_forecast(data, conf=None, times=None, forecast_count=None, save_location=None):
     '''
     This function produces 4-panel figures 
     '''
     # ------------------------------ #
     # visualization settings
-    ## variable range limit with units of m/s, m/s, K, g/kg
-    var_lims = [[-20, 20], [-20, 20], [273.15-35, 273.15+35], [0, 1e-2]]
     ## colormap
-    colormaps = [plt.cm.Spectral, plt.cm.Spectral, plt.cm.RdBu_r, plt.cm.YlGn]
-    ## colorbar extend
-    colorbar_extends = ['both', 'both', 'both', 'max']
-    ## title
-    title_strings = ['U wind [m/s]; level {}\ntime: {}; step: {}', 
-                     'V wind [m/s]; level {}\ntime: {}; step: {}', 
-                     'Air temperature [$^\circ$K]; level {}\ntime: {}; step: {}', 
-                     'Specific humidity [kg/kg]; level {}\ntime: {}; step: {}']
+    colormaps = get_colormap(conf['visualization']['upper_air_visualize']['colormaps'])
+    ## variable names
+    var_names = conf['visualization']['upper_air_visualize']['variable_names']
+    title_string = '{}; level {}\ntime: {}; step: {}' #.format(var_name, level, datetime, step)
+    ## number of levels
+    N_levels = int(len(conf['loss']['variable_weights']['U']))
+    ## number of variables to plot
+    var_num = int(len(var_names))
+    level_nums = conf['visualization']['upper_air_visualize']['visualize_levels']
+    ## output figure options and names
+    save_options = conf['visualization']['upper_air_visualize']['save_options']
+    save_name_head = conf['visualization']['upper_air_visualize']['file_name_prefix']
+    ## collect figure names
+    filenames = []
+    
     # ------------------------------ #
     # get forecast step and file name
     k, fn = data
@@ -56,58 +112,63 @@ def draw_forecast(data, N_level=15, level_num=10, var_num=4,
     longitude = lat_lon_weights["longitude"]
     latitude = lat_lon_weights["latitude"]
     
-    # ------------------------------ #
-    # Figure
-    fig = plt.figure(figsize=(13, 6.5))
     
-    # 2-by-2 subplots
-    gs = gridspec.GridSpec(2, 2, height_ratios=[1, 1], width_ratios=[1, 1])
-    proj_ = ccrs.EckertIII()
+    for level_num in level_nums:
+        # ------------------------------ #
+        # Figure
+        fig = plt.figure(figsize=(13, 6.5))
+        
+        # 2-by-2 subplots
+        gs = gridspec.GridSpec(2, 2, height_ratios=[1, 1], width_ratios=[1, 1])
+        proj_ = ccrs.EckertIII()
+        
+        # subplot ax
+        ax0 = plt.subplot(gs[0, 0], projection=proj_)
+        ax1 = plt.subplot(gs[0, 1], projection=proj_)
+        ax2 = plt.subplot(gs[1, 0], projection=proj_)
+        ax3 = plt.subplot(gs[1, 1], projection=proj_)
+        AX = [ax0, ax1, ax2, ax3]
+        plt.subplots_adjust(0, 0, 1, 1, hspace=0.2, wspace=0.05)
+        
+        # lat/lon gridlines and labeling
+        for ax in AX:
+            GL = ax.gridlines(crs=ccrs.PlateCarree(), 
+                              draw_labels=True, x_inline=False, y_inline=False, 
+                              color='k', linewidth=0.5, linestyle=':', zorder=5)
+            GL.top_labels = None; GL.bottom_labels = None
+            GL.right_labels = None; GL.left_labels = None
+            GL.xlabel_style = {'size': 14}; GL.ylabel_style = {'size': 14}
+            GL.rotate_labels = False
+        
+            ax.add_feature(cfeature.COASTLINE.with_scale('110m'), edgecolor='k', linewidth=1.0, zorder=5)
+            ax.spines['geo'].set_linewidth(2.5)
+        
+        # pcolormesh / colorbar / title in loops
+        for i_var in range(var_num):
+            # get the current axis
+            ax = AX[i_var]
+            # get the current variable
+            var_ind = i_var*N_levels + level_num
+            pred_draw = pred[var_ind]
+            # get visualization settings
+            var_lim = get_variable_range(pred_draw)
+            cbar_extend = get_colormap_extend(var_lim)
+            colormap = colormaps[i_var]
+            # pcolormesh
+            cbar = ax.pcolormesh(longitude, latitude, pred_draw, vmin=var_lim[0], vmax=var_lim[1], 
+                                 cmap=colormap, transform=ccrs.PlateCarree())
+            # colorbar operations
+            CBar = fig.colorbar(cbar, location='right', orientation='vertical', 
+                                pad=0.02, fraction=0.025, shrink=0.6, aspect=15, extend=cbar_extend, ax=ax)
+            CBar.ax.tick_params(axis='y', labelsize=14, direction='in', length=0)
+            CBar.outline.set_linewidth(2.5)
+            # title
+            var_name = var_names[i_var]
+            ax.set_title(title_string.format(var_name, level_num, t, k), fontsize=14)
     
-    # subplot ax
-    ax0 = plt.subplot(gs[0, 0], projection=proj_)
-    ax1 = plt.subplot(gs[0, 1], projection=proj_)
-    ax2 = plt.subplot(gs[1, 0], projection=proj_)
-    ax3 = plt.subplot(gs[1, 1], projection=proj_)
-    AX = [ax0, ax1, ax2, ax3]
-    plt.subplots_adjust(0, 0, 1, 1, hspace=0.2, wspace=0.05)
-    
-    # lat/lon gridlines and labeling
-    for ax in AX:
-        GL = ax.gridlines(crs=ccrs.PlateCarree(), 
-                          draw_labels=True, x_inline=False, y_inline=False, 
-                          color='k', linewidth=0.5, linestyle=':', zorder=5)
-        GL.top_labels = None; GL.bottom_labels = None
-        GL.right_labels = None; GL.left_labels = None
-        GL.xlabel_style = {'size': 14}; GL.ylabel_style = {'size': 14}
-        GL.rotate_labels = False
-    
-        ax.add_feature(cfeature.COASTLINE.with_scale('110m'), edgecolor='k', linewidth=1.0, zorder=5)
-        ax.spines['geo'].set_linewidth(2.5)
-
-    # pcolormesh / colorbar / title in loops
-    for i_var in range(var_num):
-        # get the current axis
-        ax = AX[i_var]
-        # get the current variable
-        var_ind = i_var*N_level + level_num
-        pred_draw = pred[var_ind]
-        # get visualization settings
-        var_lim = var_lims[i_var]
-        colormap = colormaps[i_var]
-        cbar_extend = colorbar_extends[i_var]
-        # pcolormesh
-        cbar = ax.pcolormesh(longitude, latitude, pred_draw, vmin=var_lim[0], vmax=var_lim[1], 
-                             cmap=colormap, transform=ccrs.PlateCarree())
-        # colorbar operations
-        CBar = fig.colorbar(cbar, location='right', orientation='vertical', 
-                            pad=0.02, fraction=0.025, shrink=0.6, aspect=15, extend=cbar_extend, ax=ax)
-        CBar.ax.tick_params(axis='y', labelsize=14, direction='in', length=0)
-        CBar.outline.set_linewidth(2.5)
-        # title
-        ax.set_title(title_strings[i_var].format(level_num, t, k), fontsize=14)
-    
-    filename = join(save_location, f"global_q_{forecast_count}_{k}.png")
-    plt.savefig(filename, dpi=300, bbox_inches="tight")
-    plt.close()
-    return k, filename
+        save_name = '{}_level{:02d}_fcst{:03d}_step{:03d}.png'.format(save_name_head, level_num, forecast_count, k)
+        filename = join(save_location, save_name)
+        plt.savefig(filename, **save_options)
+        plt.close()
+        filenames.append(filename)
+    return k, filenames
