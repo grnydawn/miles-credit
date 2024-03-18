876582e7832c0a9fa655355452891d676b66679e
djgagne@ou.edu <davidjohngagne@gmail.com>
2024-03-18 17:48:26 -0600

    Got edge calculation working

--- applications/graph_edges.py
+++ applications/graph_edges.py
@@ -22,31 +22,38 @@ def main():
     lon_grid, lat_grid = np.meshgrid(lon, lat)
     lon_flat = lon_grid.ravel()
     lat_flat = lat_grid.ravel()
-    coord_set = [(i, lon_flat[i], lat_flat[i]) for i in range(lon_flat.size)]
+    print("Size:", lon_flat.size)
+    chunk_size = int(lon_flat.size / args.procs)
+    coord_set = np.array([[i, lat_flat[i], lon_flat[i]] for i in range(lon_flat.size)])
     calc_edge_p = partial(calc_edges, all_coords=coord_set, max_dist=args.dist)
-    with Pool(processes=args.procs) as p:
-        results = p.map(calc_edge_p, coord_set)
+    p = Pool(processes=args.procs, maxtasksperchild=1)
+    results = p.map(calc_edge_p, coord_set, chunksize=chunk_size)
+    p.close()
+    p.join()
     edge_indices_list = [x[0] for x in results]
     dist_list = [x[1] for x in results]
-    edge_indices_arr = np.concatenate(edge_indices_list)
-    dist_arr = np.concatenate(dist_list)
-    output_ds = xr.Dataset({"edges": (("index", "pair"), edge_indices_arr),
-                            "distances": (("index", ), dist_arr),
+    edge_indices_arr = np.vstack(edge_indices_list)
+    dist_arr = np.concatenate(dist_list).ravel()
+    output_ds = xr.Dataset({"edges": (("node", "pair"), edge_indices_arr),
+                            "distances": (("node", ), dist_arr),
                 "longitude": (("index", ), lon_flat),
                 "latitude": (("index", ), lat_flat),
                 }, coords={"index": list(range(lon_flat.size))},
                 attrs=dict(coord_file=args.coord, max_distance=args.dist))
     if not exists(args.out):
         makedirs(args.out)
-    output_ds.to_netcdf(join(args.out, f"grid_edge_pairs_{args.dist:0.1f}.nc"))
+    print("Saving to " + args.out)
+    output_ds.to_netcdf(join(args.out, f"grid_edge_pairs_{args.dist:0.0f}.nc"))
     return
 
 
 def calc_edges(coord, all_coords=None, max_dist=25.0):
-    coord_distances = haversine_vector(coord[1:], all_coords[:, 1:], Unit.KILOMETERS)
+    coord_distances = haversine_vector(coord[1:], all_coords[:, 1:], Unit.KILOMETERS, comb=True)
     close_coords = np.where(coord_distances < max_dist)[0]
     edge_distances = coord_distances[close_coords]
-    edge_indices = [(int(coord[0]), c) for c in close_coords]
+    edge_indices = [(int(np.round(coord[0])), c) for c in close_coords] 
+    if coord[0] % 100 == 0:
+        print(coord, edge_distances.size)
     return edge_indices, edge_distances
 
 if __name__ == "__main__":
