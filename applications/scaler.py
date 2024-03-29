4dcb35c8758438fdd1541c9be8b7231643abbd4f
David John Gagne <davidjohngagne@gmail.com>
2024-03-29 10:28:36 -0600

    Removed multiprocessing from scaler.py.

--- applications/scaler.py
+++ applications/scaler.py
@@ -6,46 +6,50 @@ import yaml
 import argparse
 from glob import glob
 from bridgescaler.distributed import DQuantileScaler
-from bridgescaler.backend import print_scaler
 from os.path import exists, join
 from mpi4py import MPI
-from multiprocessing import Pool
-from functools import partial
 from time import time
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="Path to config file")
     parser.add_argument("-o", "--out", help="Path to save scaler files.")
-    parser.add_argument("-t", "--time", type=int, default=1752, help="Number of hours sampled/year")
-
-    parser.add_argument("-p", "--procs", type=int, default=1, help="Number of processors")
+    parser.add_argument("-t", "--time", type=str, default="25h",
+                        help="Difference between times used for fitting.")
     args = parser.parse_args()
     args_dict = vars(args)
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-
+    config = args_dict.pop("config")
+    with open(config) as cf:
+        conf = yaml.load(cf, Loader=yaml.FullLoader)
     if rank == 0:
-        config = args_dict.pop("config")
-        with open(config) as cf:
-            conf = yaml.load(cf, Loader=yaml.FullLoader)
         all_era5_files = sorted(glob(conf["data"]["save_loc"]))
         for e5 in all_era5_files:
             if "_small_" in e5:
                 all_era5_files.remove(e5)
         all_era5_filenames = np.array([f.split("/")[-1] for f in all_era5_files])
+        era5_dates = []
+        for fname in all_era5_filenames:
+            start_date_str, end_date_str = fname.split("_")[1:2]
+            start_date_str += "_00:00"
+            end_date_str += "_23:00"
+            era5_dates.append(pd.date_range(start=start_date_str, end=end_date_str, freq=args_dict["time"]))
+        all_era5_dates = pd.concat(era5_dates)
+        split_indices = np.linspace(0, all_era5_dates.size, size + 1)
+        split_era5_dates = [all_era5_dates.values[split_indices[s]:split_indices[s + 1]]
+                            for s in range(split_indices.size - 1)]
     else:
-        all_era5_files = None
         all_era5_filenames = None
-    era5_subset = comm.scatter(all_era5_files, root=0)
-    print(f"Rank {rank:d}", era5_subset, type(era5_subset))
-    if type(era5_subset) == list:
-        scalers = []
-        for era5_file in era5_subset:
-            scalers.append(fit_era5_scaler_year(era5_file, n_times=args.time, n_jobs=args.procs))
-    else:
-        scalers = fit_era5_scaler_year(era5_subset, n_times=args.time, n_jobs=args.procs)
+        split_era5_dates = None
+    era5_subset_times = comm.scatter(split_era5_dates, root=0)
+    vars_3d = conf["data"]["variables"]
+    vars_surf = conf["data"]["surface_variables"]
+    e5_file_dir = "/".join(conf["data"]["save_loc"].split("/")[:-1])
+    scalers = fit_era5_scaler_times(era5_subset_times, rank, era5_file_dir=e5_file_dir,
+                                    vars_3d=vars_3d, vars_surf=vars_surf)
     all_scalers = comm.gather(scalers, root=0)
     if rank == 0:
         all_scalers_df = pd.DataFrame(all_scalers, columns=["scaler_3d", "scaler_surface"],
@@ -57,49 +61,43 @@ def main():
     return
 
 
-def fit_era5_scaler_year(era5_file, n_times=60, n_jobs=60):
-    pool = Pool(n_jobs, maxtasksperchild=1) 
-    eds = xr.open_zarr(era5_file)
-    vars_3d = ['U', 'V', 'T', 'Q']
-    vars_surf = ['SP', 't2m', 'V500', 'U500', 'T500', 'Z500', 'Q500']
+def fit_era5_scaler_times(times, rank, era5_file_dir=None, vars_3d=None, vars_surf=None):
+    dqs_3d = DQuantileScaler(distribution="normal", channels_last=False)
+    dqs_surf = DQuantileScaler(distribution="normal", channels_last=False)
+    curr_f_start = pd.Timestamp(times[0].strftime("%Y") + "-01-01_00:00")
+    curr_f_end = pd.Timestamp(times[0].strftime("%Y") + "-12-31_23:00")
+    curr_f_start_str = curr_f_start.strftime("%Y-%m-%d")
+    curr_f_end_str = curr_f_end.strftime("%Y-%m-%d")
+    eds = xr.open_zarr(join(era5_file_dir, f"TOTAL_{curr_f_start_str}_{curr_f_end_str}_staged.zarr"))
     levels = eds.level.values
     var_levels = []
     for var in vars_3d:
         for level in levels:
             var_levels.append(f"{var}_{level:d}")
-    rand_times = np.sort(np.random.choice(eds["time"].values, size=n_times, replace=False))
-    fit_time_func = partial(fit_era5_scaler_time, era5_file=era5_file, vars_3d=vars_3d, vars_surf=vars_surf, 
-                            levels=levels, var_levels=var_levels, times=rand_times)
-    results = np.array(pool.map(fit_time_func, rand_times))
-    dqs_3d = np.sum(results[:, 0])
-    dqs_surf = np.sum(results[:, 1])
-    dqs_3d_json = print_scaler(dqs_3d)
-    dqs_surf_json = print_scaler(dqs_surf)
-    pool.close()
-    pool.join()
-    return dqs_3d_json, dqs_surf_json
-
-
-def fit_era5_scaler_time(time, era5_file=None, vars_3d=None, vars_surf=None, levels=None, var_levels=None, times=None):  
-    eds = xr.open_zarr(era5_file)
-    dqs_3d = DQuantileScaler(distribution="normal", channels_last=False)
-    dqs_surf = DQuantileScaler(distribution="normal", channels_last=False)
-    var_slices = []
-    for var in vars_3d:
-        for level in levels:
-            var_slices.append(eds[var].loc[time, level])
-    e3d = xr.concat(var_slices, pd.Index(var_levels, name="variable")
-                        ).load()
-    e3d = e3d.expand_dims(dim="time", axis=0)
-    print(time, np.searchsorted(times, time), times.size)
-    dqs_3d.fit(e3d)
-    e_surf = xr.concat([eds[v].loc[time] for v in vars_surf], pd.Index(vars_surf, name="variable")
+    n_times = times.size
+    for t, ctime in enumerate(times):
+        print(f"Rank {rank:d}: {ctime} {t:d}/{n_times:d}")
+        if not curr_f_start >= ctime <= curr_f_end:
+            eds.close()
+            curr_f_start = pd.Timestamp(times[0].strftime("%Y") + "-01-01_00:00")
+            curr_f_end = pd.Timestamp(times[0].strftime("%Y") + "-12-31_23:00")
+            curr_f_start_str = curr_f_start.strftime("%Y-%m-%d")
+            curr_f_end_str = curr_f_end.strftime("%Y-%m-%d")
+            eds = xr.open_zarr(join(era5_file_dir, f"TOTAL_{curr_f_start_str}_{curr_f_end_str}_staged.zarr"))
+        var_slices = []
+        for var in vars_3d:
+            for level in levels:
+                var_slices.append(eds[var].loc[time, level])
+        e3d = xr.concat(var_slices, pd.Index(var_levels, name="variable")).load()
+        e3d = e3d.expand_dims(dim="time", axis=0)
+        dqs_3d.fit(e3d)
+        e_surf = xr.concat([eds[v].loc[time] for v in vars_surf], pd.Index(vars_surf, name="variable")
                            ).load()
-    e_surf = e_surf.expand_dims(dim="time", axis=0)
-    dqs_surf.fit(e_surf)
+        e_surf = e_surf.expand_dims(dim="time", axis=0)
+        dqs_surf.fit(e_surf)
     eds.close()
-    del eds
     return dqs_3d, dqs_surf
+
 
 if __name__ == '__main__':
     main()
