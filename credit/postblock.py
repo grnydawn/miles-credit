ae903762f25050dfcf3e875bba294444fe9560c0
Yingkai Sha <yingkaisha@gmail.com>
2024-10-01 13:33:33 -0600

    adding negative tracer fixer to postblock.py

--- credit/postblock.py
+++ credit/postblock.py
@@ -17,15 +17,67 @@ class PostBlock(nn.Module):
         super().__init__()
 
         self.operations = nn.ModuleList()
+        
         if post_conf["use_skebs"]:
             logging.info("using SKEBS")
             self.operations.append(SKEBS(post_conf))
+
+        # negative tracer fixer
+        if post_conf['tracer_fixer']['activate']:
+            opt = tracer_fixer(post_conf)
+            self.operations.append(opt)
+
+        if post_conf["use_skebs"]:
 
     def forward(self, x):
         for op in self.operations:
             x = op(x)
         return x
-    
+
+class tracer_fixer(nn.Module):
+    '''
+    This class non-negative tracers by replacing their negative values to zero.
+    Modification is done witohut making copies.
+    '''
+    def __init__(self, post_conf):
+        super().__init__()
+        
+        self.tracer_indices = conf['model']['post_conf']['tracer_fixer']['tracer_inds']
+        
+    def forward(self, x):
+        # negtive tracer correction
+        for i_var in self.tracer_indices
+            # y_pred is channel first: (batch, var, time, lat, lon)
+            tracer_vals = x["y_pred"][:, i_var, ...]
+            
+            # modify `x["y_pred"][:, i_var, ...]` in-place
+            tracer_vals[tracer_vals<0] = 0
+            
+        return x
+
+# class global_mass_fixer(nn.Module):
+#     '''
+#     '''
+#     def __init__(self, post_conf):
+#         super().__init__()
+
+#     def forward(self, x):
+#         x_input = x['x']
+#         y_pred = x["y_pred"]
+#         ##
+
+# class global_energy_fixer(nn.Module):
+#     '''
+#     '''
+#     def __init__(self, post_conf):
+#         super().__init__()
+
+#     def forward(self, x):
+#         x_input = x['x']
+#         y_pred = x["y_pred"]
+#         ##
+
+        
 class SKEBS(nn.Module):
     """
         post_conf: dictionary with config options for PostBlock.
