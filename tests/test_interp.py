fef6417a94dff6d30d0fde5645c7aebdea549f7e
djgagne@ou.edu <davidjohngagne@gmail.com>
2024-10-22 18:16:40 -0600

    Added level info and test data for interpolation

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -1,5 +1,22 @@
 from credit.interp import full_state_pressure_interpolation
+import xarray as xr
+import os
+import numpy as np
 
 
 def test_full_state_pressure_interpolation():
+    input_file = os.path.abspath("data/test_interp.nc")
+    model_level_file = os.path.abspath("../credit/metadata/ERA5_Lev_Info.nc")
+    ds = xr.open_dataset(input_file)
+    model_levels = xr.open_dataset(model_level_file)
+    pressure_levels = np.array([200.0, 500.0, 700.0, 850.0, 1000.0]) * 100.0
+    model_a = model_levels["a_model"].loc[ds_sub["level"]].values
+    model_b = model_levels["b_model"].loc[ds_sub["level"]].values
+    interp_ds = full_state_pressure_interpolation(ds, 
+                                                  pressure_levels, 
+                                                  model_a, 
+                                                  model_b,
+                                                  lat_var="lat",
+                                                  lon_var="lon")
+    assert interp_ds["U"].shape[1] == pressure_levels.size, "Pressure level mismatch"
     return
