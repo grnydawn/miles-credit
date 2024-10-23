1269d14041eb11002b7297d2757d671f4483693d
djgagne@ou.edu <davidjohngagne@gmail.com>
2024-10-22 18:32:30 -0600

    Added test interp test

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -5,13 +5,14 @@ import numpy as np
 
 
 def test_full_state_pressure_interpolation():
-    input_file = os.path.abspath("data/test_interp.nc")
-    model_level_file = os.path.abspath("../credit/metadata/ERA5_Lev_Info.nc")
+    path_to_test = os.path.abspath(os.path.dirname(__file__))
+    input_file = os.path.join(path_to_test, "data/test_interp.nc")
+    model_level_file = os.path.join(path_to_test, "../credit/metadata/ERA5_Lev_Info.nc")
     ds = xr.open_dataset(input_file)
     model_levels = xr.open_dataset(model_level_file)
     pressure_levels = np.array([200.0, 500.0, 700.0, 850.0, 1000.0]) * 100.0
-    model_a = model_levels["a_model"].loc[ds_sub["level"]].values
-    model_b = model_levels["b_model"].loc[ds_sub["level"]].values
+    model_a = model_levels["a_model"].loc[ds["level"]].values
+    model_b = model_levels["b_model"].loc[ds["level"]].values
     interp_ds = full_state_pressure_interpolation(ds, 
                                                   pressure_levels, 
                                                   model_a, 
