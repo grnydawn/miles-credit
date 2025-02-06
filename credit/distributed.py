36b6504dbab082974a2cdb702af533e374cf6c20
Arnold Kazadi <arnoldnyamabo@gmail.com>
2025-02-06 08:34:19 -0700

    Removed find_unused_parameters=True from DDP wrapper

--- credit/distributed.py
+++ credit/distributed.py
@@ -232,7 +232,8 @@ def distributed_model_wrapper(conf, neural_network, device):
         )
 
     elif conf["trainer"]["mode"] == "ddp":
-        model = DDP(neural_network, device_ids=[device], find_unused_parameters=True) 
+        model = DDP(neural_network, device_ids=[device]) 
+        # model = DDP(neural_network, device_ids=[device], find_unused_parameters=True) # Use this if you have a model with layers/modules/parameters unused in the forward method
 
     else:
         model = neural_network
