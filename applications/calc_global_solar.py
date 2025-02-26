c0bdc842277ecbb71be0e37813d0f8c93e5bcb7c
David John Gagne <davidjohngagne@gmail.com>
2025-02-26 16:14:14 -0700

    Updated solar to output zarr.

--- applications/calc_global_solar.py
+++ applications/calc_global_solar.py
@@ -12,12 +12,8 @@ def main():
     rank = comm.Get_rank()
     size = comm.Get_size()
     parser = argparse.ArgumentParser(description="Calculate global solar radiation")
-    parser.add_argument(
-        "-s", "--start", type=str, default="2000-01-01", help="Start date (inclusive)"
-    )
-    parser.add_argument(
-        "-e", "--end", type=str, default="2000-12-31 23:00", help="End date (inclusive"
-    )
+    parser.add_argument("-s", "--start", type=str, default="2000-01-01", help="Start date (inclusive)")
+    parser.add_argument("-e", "--end", type=str, default="2000-12-31 23:00", help="End date (inclusive")
     parser.add_argument("-t", "--step", type=str, default="1h", help="Step frequency")
     parser.add_argument("-u", "--sub", type=str, default="1Min", help="Sub Frequency")
     parser.add_argument(
@@ -34,9 +30,8 @@ def main():
         default="Z_GDS4_SFC",
         help="Geopotential height variable.",
     )
-    parser.add_argument(
-        "-o", "--output", type=str, required=True, help="Output directory"
-    )
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output directory")
+    parser.add_argument("-z", "--zarr", type=str, action="store_true", help="Output as zarr files.")
     args = parser.parse_args()
     grid_points_sub = None
     start_date_ts = pd.Timestamp(args.start)
@@ -55,15 +50,10 @@ def main():
                 attrs={"long_name": "total solar irradiance", "units": "J m-2"},
             )
             heights = static_ds[args.geo].values / 9.81
-            grid_points = np.vstack(
-                [lon_grid.ravel(), lat_grid.ravel(), heights.ravel()]
-            ).T
-            split_indices = np.round(
-                np.linspace(0, grid_points.shape[0], size + 1)
-            ).astype(int)
+            grid_points = np.vstack([lon_grid.ravel(), lat_grid.ravel(), heights.ravel()]).T
+            split_indices = np.round(np.linspace(0, grid_points.shape[0], size + 1)).astype(int)
             grid_points_sub = [
-                grid_points[split_indices[s] : split_indices[s + 1]]
-                for s in range(split_indices.size - 1)
+                grid_points[split_indices[s] : split_indices[s + 1]] for s in range(split_indices.size - 1)
             ]
     rank_points = comm.scatter(grid_points_sub, root=0)
     print(rank_points.shape)
@@ -92,13 +82,9 @@ def main():
                 tag=rank,
             )
         else:
-            solar_grid.loc[:, solar_point["latitude"], solar_point["longitude"]] = (
-                solar_point["tsi"].values
-            )
+            solar_grid.loc[:, solar_point["latitude"], solar_point["longitude"]] = solar_point["tsi"].values
             for sr in range(1, size):
-                other_point = np.empty(
-                    2 + solar_grid.shape[0], dtype=solar_point["tsi"].dtype
-                )
+                other_point = np.empty(2 + solar_grid.shape[0], dtype=solar_point["tsi"].dtype)
                 comm.Recv(other_point, source=sr, tag=sr)
                 solar_grid.loc[:, other_point[0], other_point[1]] = other_point[2:]
 
@@ -110,23 +96,39 @@ def main():
         date_format = "%Y-%m-%d_%H%M"
         start_date_str = start_date_ts.strftime(date_format)
         end_date_str = end_date_ts.strftime(date_format)
-        filename = f"solar_irradiance_{start_date_str}_{end_date_str}.nc"
         print("Saving")
-        solar_grid.to_netcdf(
-            os.path.join(args.output, filename),
-            encoding={
-                "tsi": {
-                    "zlib": True,
-                    "complevel": 1,
-                    "shuffle": True,
-                    "chunksizes": (
-                        1,
-                        solar_grid.shape[1],
-                        solar_grid.shape[2],
-                    ),
-                }
-            },
-        )
+        if args.zarr:
+            filename = f"solar_irradiance_{start_date_str}_{end_date_str}.zarr"
+            solar_grid.to_zarr(
+                os.path.join(args.output, filename),
+                mode="w",
+                encoding={
+                    "tsi": {
+                        "chunksizes": (
+                            1,
+                            solar_grid.shape[1],
+                            solar_grid.shape[2],
+                        )
+                    }
+                },
+            )
+        else:
+            filename = f"solar_irradiance_{start_date_str}_{end_date_str}.nc"
+            solar_grid.to_netcdf(
+                os.path.join(args.output, filename),
+                encoding={
+                    "tsi": {
+                        "zlib": True,
+                        "complevel": 1,
+                        "shuffle": True,
+                        "chunksizes": (
+                            1,
+                            solar_grid.shape[1],
+                            solar_grid.shape[2],
+                        ),
+                    }
+                },
+            )
     return
 
 
