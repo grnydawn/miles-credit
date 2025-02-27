ce391cd9a6bc94eea73d9e5ab1843a22f71736f8
charlie-becker <cbecker@ucar.edu>
2025-02-27 16:42:19 -0700

    clean up / some bugs

--- applications/gfs_init.py
+++ applications/gfs_init.py
@@ -3,7 +3,6 @@ from credit import metadata
 import yaml
 import argparse
 import xarray as xr
-import importlib.resources
 
 
 parser = argparse.ArgumentParser()
@@ -12,15 +11,16 @@ args = parser.parse_args()
 with open(args.config) as config_file:
     config = yaml.safe_load(config_file)
 
-with importlib.resources.path(metadta, "ERA5_Lev_Info.nc") as nc_path:
-    credit_grid = xr.open_dataset(nc_path)
+credit_grid = xr.open_dataset('/glade/work/cbecker/miles-credit/credit/metadata/ERA5_Lev_Info.nc')
 
 date = "202502200600"
 model_levels = [ 10,  30,  40,  50,  60,  70,  80,  90,  95, 100, 105, 110, 120, 130, 136, 137]
-
 variables = config['data']['variables'] + config['data']['surface_variables']
+
 gfs_init = build_GFS_init(output_grid=credit_grid,
                           variables=variables,
                           model_level_indices=model_levels)
 
-gfs_init.to_zarr("/gladE/derecho/scratch/cbecker/gfs_init.zarr")
+print(gfs_init)
+# gfs_init.to_netcdf("/glade/derecho/scratch/cbecker/gfs_init.nc")
+gfs_init.to_zarr("/glade/derecho/scratch/cbecker/gfs_init.zarr")
--- credit/nwp.py
+++ credit/nwp.py
@@ -1,24 +1,31 @@
 import numpy as np
 import pandas as pd
 from os.path import join
+import xarray as xr
+import fsspec
+import xesmf as xe
 
 var_map = {'tmp': 'T', 'ugrd': 'U', 'vgrd': 'V', 'spfh': 'Q', 'pressfc': 'SP', 'tmp2m': 't2m'}
+level_map = {'T500': 'tmp', 'U500': 'ugrd', 'V500': 'vgrd', 'Q500': 'spfh', 'Z500': 'Z'}
 
 def build_GFS_init(output_grid, gdas_base_path="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/",
-                   date="202502200600", variables=['tmp', 'ugrd', 'vgrd', 'pressfc', 'tmp2m'], model_level_indices=[]):
+                   date="202502200600", variables=[], model_level_indices=[]):
+
+    variables = [k for k, v in var_map.items() if v in variables]
     atm_full_path = build_file_path(date, gdas_base_path, file_type='atm')
     sfc_full_path = build_file_path(date, gdas_base_path, file_type='sfc')
     gfs_atm_data = load_gfs_data(atm_full_path, variables)
     gfs_sfc_data = load_gfs_data(sfc_full_path, variables)
     gfs_data = combine_data(gfs_atm_data, gfs_sfc_data)
     regridded_gfs = regrid(gfs_data, output_grid)
-    interpolated_gfs = interpolate_to_model_level(regridded_gfs, output_grid, model_level_indices)
-    final_data = format_data(interpolated_gfs, var_map)
+    interpolated_gfs = interpolate_to_model_level(regridded_gfs, output_grid, model_level_indices, variables)
+    final_data = format_data(interpolated_gfs, regridded_gfs, model_level_indices)
 
-    return gfs_data, regridded_gfs, interpolated_gfs, final_data
+    return final_data
 
 
 def build_file_path(date, base_path, file_type='atm'):
+
     date_obj = pd.Timestamp(date)
     dir_path = date_obj.strftime("gdas.%Y%m%d/%H/atmos/")
     file_name = date_obj.strftime(f"gdas.t%Hz.{file_type}anl.nc")
@@ -27,13 +34,19 @@ def build_file_path(date, base_path, file_type='atm'):
 
 
 def load_gfs_data(full_file_path, variables):
+
     ds = xr.open_dataset(fsspec.open(full_file_path).open())
     available_vars = ds.data_vars
     vars = [v for v in variables if v in available_vars]
-    return ds[vars].rename({'grid_xt': 'longitude', 'grid_yt': 'latitude'}).load()
+    ds = ds[vars].rename({'grid_xt': 'longitude', 'grid_yt': 'latitude'}).load()
+    if 'delz' in available_vars and 'Z500' in variables:
+        ds['delz'].values = np.abs(ds['delz'].cumsum(dim='pfull').values)
+        ds = ds.rename({'delz': 'Z'})
+    return ds
 
 
 def combine_data(atm_data, sfc_data):
+
     for var in sfc_data.data_vars:
         atm_data[var] = (sfc_data[var].dims, sfc_data[var].values)
 
@@ -41,6 +54,7 @@ def combine_data(atm_data, sfc_data):
 
 
 def regrid(nwp_data, output_grid, method="conservative"):
+
     ds_out = output_grid[['longitude', 'latitude']].drop_vars(['time']).load()
     in_grid = nwp_data[['longitude', 'latitude']].load()
     regridder = xe.Regridder(in_grid, ds_out, method=method)
@@ -49,7 +63,8 @@ def regrid(nwp_data, output_grid, method="conservative"):
     return ds_regridded.squeeze()
 
 
-def interpolate_to_model_level(regridded_nwp_data, output_grid, model_level_indices):
+def interpolate_to_model_level(regridded_nwp_data, output_grid, model_level_indices, variables):
+
     xp = regridded_nwp_data['pfull'].values
     fp = regridded_nwp_data
     output_pressure = (output_grid['a_half'] + output_grid['b_half'] * regridded_nwp_data['pressfc']) / 100
@@ -57,13 +72,16 @@ def interpolate_to_model_level(regridded_nwp_data, output_grid, model_level_indi
     ny, nx = fp['tmp'].shape[1], fp['tmp'].shape[2]
     interpolated_data = {}
     for var in ['tmp', 'ugrd', 'vgrd']:
-        print(var)
+        if var not in variables:
+            continue
         fp_data = fp[var].values
         interpolated_data[var] = {'dims': ['latitude', 'longitude', 'level'],
                                   'data': np.array([np.interp(sampled_output_pressure[:, j, i], xp, fp_data[:, j, i])
                                                     for j in range(ny) for i in range(nx)]).reshape(ny, nx,
                                                                                                     len(model_level_indices))}
-    for var, var1 in zip(['T500', 'U500', 'V500'], ['tmp', 'ugrd', 'vgrd']):
+    # for var, var1 in zip(['T500', 'U500', 'V500', 'Z500'], ['tmp', 'ugrd', 'vgrd', 'Z']):
+    vars_500 = [var for var in variables if '500' in var]
+    for var, var1 in zip(vars_500, [level_map[k] for k in vars_500]):
         fp_data = fp[var1].values
         interpolated_data[var] = {'dims': ['latitude', 'longitude'],
                                   'data': np.array([np.interp([500], xp, fp_data[:, j, i])
@@ -74,11 +92,18 @@ def interpolate_to_model_level(regridded_nwp_data, output_grid, model_level_indi
     return interpolated_data
 
 
-def format_data(data_dict, var_map):
+def format_data(data_dict, regridded_data, model_levels):
+
     data = xr.Dataset.from_dict(data_dict).transpose('level', 'latitude', 'longitude', ...).expand_dims('time')
     for v in data.data_vars:
         if v in var_map.keys():
             data = data.rename({v: var_map[v]})
+
+    data = data.assign_coords(level=model_levels,
+                              latitude=regridded_data['latitude'].values,
+                              longitude=regridded_data['longitude'].values,
+                              time=[pd.to_datetime(regridded_data['time'].values.astype(str))])
+
     return data
 
 
