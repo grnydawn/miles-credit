18ef3777ce257db9dca428fd1bd0350abb7cada1
charlie-becker <cbecker@ucar.edu>
2025-03-27 16:37:45 -0600

    new conf / small refactor

--- applications/gfs_init.py
+++ applications/gfs_init.py
@@ -13,20 +13,19 @@ args = parser.parse_args()
 with open(args.config) as config_file:
     config = yaml.safe_load(config_file)
 
-os.makedirs(config['predict']['save_forecast'], exist_ok=True)
+os.makedirs(config['save_out_path'], exist_ok=True)
 base_path = os.path.abspath(os.path.dirname(__file__))
 credit_grid = xr.open_dataset(os.path.join(base_path, "credit/metadata/ERA5_Lev_Info.nc"))
 model_levels = pd.read_csv(join(base_path, "credit/metadata/L137_model_level_indices.csv"))
 model_level_indices = model_levels["model_level_indices"].values
-date = format_datetime(config["predict"]["forecasts"])
-variables = config['data']['variables'] + config['data']['surface_variables']
-
+variables = config["upper_air_variables"] + config["surface_variables"]
+date = pd.Timestamp(config["init_time"])
 gfs_init = build_GFS_init(output_grid=credit_grid,
                           date=date,
                           variables=variables,
                           model_level_indices=model_level_indices)
 
-gfs_init.to_zarr(join(base_out_path, f"gfs_init_{date.strftime('%Y%m%d_%H00')}.zarr"))
+gfs_init.to_zarr(join(config["save_out_path"], f"gfs_init_{date.strftime('%Y%m%d_%H00')}.zarr"))
 
 
 
