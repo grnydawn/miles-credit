032c5022388286863e324042c04f1c032be9c26f
John Schreck <jsschreck@gmail.com>
2025-06-10 16:25:18 -0600

    Cleaup logger in forecast.oy

--- credit/forecast.py
+++ credit/forecast.py
@@ -12,13 +12,9 @@ def load_forecasts(conf):
             conf["predict"]["forecasts"]["start_month"],
             conf["predict"]["forecasts"]["start_day"],
         )
-        start_hours = conf["predict"]["forecasts"].get(
-            "start_hours", [0]
-        )  # Default to [0] if not provided
+        start_hours = conf["predict"]["forecasts"].get("start_hours", [0])  # Default to [0] if not provided
         if forecast_type == "10day_year":
-            return generate_forecasts(
-                start_date, days=10, duration=365, start_hours=start_hours
-            )
+            return generate_forecasts(start_date, days=10, duration=365, start_hours=start_hours)
         elif forecast_type == "custom":
             days = conf["predict"]["forecasts"].get("days", 10)
             lead_time_periods = conf["data"].get("lead_time_periods", 1)
@@ -40,9 +36,7 @@ def load_forecasts(conf):
 
 
 # Function to generate forecasts for specified duration
-def generate_forecasts(
-    start_date, lead_time_periods=1, days=10, duration=365, start_hours=(0), ic_interval_days=1
-):
+def generate_forecasts(start_date, lead_time_periods=1, days=10, duration=365, start_hours=(0), ic_interval_days=1):
     """
     lead_time_periods = 1 for hourly forecast; =6 for 6 hourly forecast
     """
@@ -53,9 +47,7 @@ def generate_forecasts(
     for _ in range(duration // ic_interval_days):
         for hour in start_hours:
             start_datetime = current_date + timedelta(hours=hour)
-            end_datetime = start_datetime + timedelta(
-                days=days - 1, hours=24 - lead_time_periods
-            )
+            end_datetime = start_datetime + timedelta(days=days - 1, hours=24 - lead_time_periods)
             forecasts.append(
                 [
                     start_datetime.strftime("%Y-%m-%d %H:%M:%S"),
@@ -66,8 +58,8 @@ def generate_forecasts(
 
     if len(forecasts) == 0:
         raise RuntimeError("No forecasts generated")
-    
-    logger.info(f"Generated{len(forecasts)} unique forecast periods ")
+
+    logger.info(f"Generated {len(forecasts)} unique forecast periods ")
 
     return forecasts
 
