23078d670caa91d858337e46fcc4b609cdcb8232
David John Gagne <davidjohngagne@gmail.com>
2025-08-14 11:07:50 -0600

    Added docs.

--- credit/models/grid.py
+++ credit/models/grid.py
@@ -4,15 +4,21 @@ import xarray as xr
 
 def scrip_from_latlon_grid(lons, lats, grid_name, grid_file):
     """
+    Generate a SCRIP netCDF file defining the centers and corners of each element in a latitude-longitude grid.
+    This function supports both equiangular lat-lon grids and full Gaussian grids.
+
+    SCRIP is a legacy software package for regridding model output. The SCRIP grid format is supported by ESMF for
+    regridding definitions. See https://github.com/SCRIP-Project/SCRIP/blob/master/SCRIP/doc/SCRIPusers.pdf for
+    details about SCRIP and
 
     Args:
-        lons:
-        lats:
-        grid_name:
-        grid_file:
+        lons: 1D array of longitudes
+        lats: 1D array of latitudes
+        grid_name: Name of the output grid map
+        grid_file: path to netCDF file containing the grid information
 
     Returns:
-
+        scrip_ds: xr.Dataset containing grid information in SCRIP format.
     """
     dlat = np.abs(lats[1:] - lats[:-1])
     dlat = np.append(dlat, dlat[-1])
@@ -50,7 +56,10 @@ def scrip_from_latlon_grid(lons, lats, grid_name, grid_file):
     lon_sign = [-1, 1, 1, -1]
     lat_sign = [-1, -1, 1, 1]
     for i in range(grid_corners):
-        grid_corner_lat[:, i] = lat_grid.ravel() + lat_sign[i] * dlat[row_indices] / 2.0
+        grid_corner_lat[:, i] = np.minimum(
+            np.maximum(lat_grid.ravel() + lat_sign[i] * dlat[row_indices] / 2.0, -90),
+            90,
+        )
         grid_corner_lon[:, i] = lon_grid.ravel() + lon_sign[i] * dlon[col_indices] / 2.0
     scrip_ds = xr.Dataset(
         {
